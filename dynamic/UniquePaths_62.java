e84966d738c1c44ec8b732933c24f72d59550d94
Maksym Radiuk <maksym.radiuk@ticketmaster.com>
2019-07-17 15:51:09 +0300

    unique pass 2

--- dynamic/UniquePaths_62.java
+++ dynamic/UniquePaths_62.java
@@ -26,27 +26,48 @@ public class UniquePaths_62 {
     public static void main(String[] args) {
         UniquePaths_62 s = new UniquePaths_62();
         System.out.println(s.uniquePaths(3,2)); //3
-        System.out.println(s.uniquePaths(7,3)); // 28
+        System.out.println(s.uniquePathsDynamic(3,2)); //3
+
+        System.out.println(s.uniquePaths(7,3)); //28
+        System.out.println(s.uniquePathsDynamic(7,3)); // 28
     }
 
     // O(2^(max(m,n)))- time, O(max{m, n}) - space
     public int uniquePaths(int m, int n) {
-        boolean[][] visited = new boolean[m][n];
-        return uniquePaths(m, n, 0, 0, visited);
+        return uniquePaths(m, n, 0, 0);
     }
 
-    private int uniquePaths(int m, int n, int i, int j, boolean[][] visited) {
-        if (i < 0 || i > m-1 || j < 0 || j > n-1 || visited[i][j]) {
+    private int uniquePaths(int m, int n, int i, int j) {
+        if (i < 0 || i > m-1 || j < 0 || j > n-1) {
             return 0;
         }
         if (i == m-1 && j == n-1) {
             return 1;
         }
 
-        int right = uniquePaths(m, n, i+1, j, visited);
-        int down = uniquePaths(m, n, i, j+1, visited);
+        int right = uniquePaths(m, n, i+1, j);
+        int down = uniquePaths(m, n, i, j+1);
 
         return right + down;
+    }
+
+    // O(m*n) - time, space
+    public int uniquePathsDynamic(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 
 }
