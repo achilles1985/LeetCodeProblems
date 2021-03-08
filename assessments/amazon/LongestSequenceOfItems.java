63e31da3a13bdf8db6429e9998906f1dcbcc2286
mradyuk <markus.rodos@gmail.com>
2021-03-08 16:31:23 +0200

    mix4 08.03.2021

--- assessments/amazon/LongestSequenceOfItems.java
+++ assessments/amazon/LongestSequenceOfItems.java
@@ -1,20 +1,55 @@
 package assessments.amazon;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+/*
+    1. Cycle is possible? self loop is possible?
+ */
 public class LongestSequenceOfItems {
 
     public static void main(String[] args) {
         LongestSequenceOfItems s = new LongestSequenceOfItems();
-        System.out.println(s.findSequence(Arrays.asList(
+        System.out.println(s.findSequence2(Arrays.asList(
                 Arrays.asList("item1", "item2"),
                 Arrays.asList("item2", "item3"),
                 Arrays.asList("item4", "item5"),
                 Arrays.asList("item3", "item6")))); //[item1, item2, item3, item6]
+    }
+
+    // O(E + V) - time, O(V) - space
+    public List<String> findSequence2(List<List<String>> itemAssociation) {
+        Map<String, List<String>> graph = new HashMap<>();
+        for (List<String> edge: itemAssociation) {
+            String from = edge.get(0);
+            String to = edge.get(1);
+            graph.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
+            graph.computeIfAbsent(to, k -> new ArrayList<>()).add(from);
+        }
+        Set<String> seen = new HashSet<>();
+        List<String> result = new ArrayList<>();
+        for (String node: graph.keySet()) {
+            if (!seen.contains(node)) {
+                List<String> nodes = new ArrayList<>();
+                dfs(node, nodes, seen, graph);
+                if (!nodes.isEmpty()) {
+                    if (nodes.size() > result.size()) {
+                        result = nodes;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private void dfs(String node, List<String> nodes, Set<String> seen, Map<String, List<String>> graph) {
+        if (seen.contains(node)) {
+            return;
+        }
+        nodes.add(node);
+        seen.add(node);
+        for (String child: graph.getOrDefault(node, new ArrayList<>())) {
+            dfs(child, nodes, seen, graph);
+        }
     }
 
     // O(n^2) - time (can be improved to O(n) - if using union-by-rank), O(n) - space , where n - number of items
@@ -22,7 +57,7 @@ public class LongestSequenceOfItems {
         UnionFind uf = new UnionFind(itemAssociation.size());
         Map<String, Integer> itemToIndex = new HashMap<>();
         int count = 0;
-        for (List<String> pair: itemAssociation) { //n
+        for (List<String> pair: itemAssociation) { //E
             for (String item: pair) {
                 if (!itemToIndex.containsKey(item)) {
                     itemToIndex.put(item, count++);
